dffe49e1193d140815ac6e168e0db429878526ea
Janos Erdos <erdosj@gmail.com>
2018-08-29 18:04:16 +0200

    Update profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [;[claravis "0.1.0-SNAPSHOT"]
                   [lein-cljfmt "0.5.7"]
+                  [lein-nodisassemble "0.1.3"]
                   [lein-hiera "1.0.0"]
                   [venantius/ultra "0.5.2"]]}}
